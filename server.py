383b6dab95082128740c84177e188d1c5fdc529c
Develthon <develthon@gmail.com>
2023-08-01 15:04:19 -0300

    Copy Right

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask("__name__")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    current_year = datetime.date.today().year
+    return render_template("index.html", year=current_year)
+
+
 if(__name__ == "__main__"):
     app.run()
